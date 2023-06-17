9ba803302c08ec08b49ec6c818ed36773782cdb4
tarun-2002 <95556480+tarun-2002@users.noreply.github.com>
2023-06-17 23:40:51 +0530

    Add files via upload

---
+++ src/pages/About/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutGB from '../../components/AboutGB'
+const AboutUs = () => {
+  return (
+    <>
+      <AboutGB/>
+      
+    </>
+  )
+}
+
+export default AboutUs
